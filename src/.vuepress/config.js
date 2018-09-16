6729bb94683dbae6909cd13ebc232f99eb37b66b
willin <willin@willin.org>
2018-09-16 14:32:26 +0800

    add config

---
+++ src/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: '初瘦',
+  description: '想当初，也瘦过。公众号：assholev0。',
+  head: [
+    ['link', { rel: 'icon', href: `/favicon.png` }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
+  // theme: 'asshole',
+  serviceWorker: true,
+  evergreen: true
+};
